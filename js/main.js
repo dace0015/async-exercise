6f3b0c32600628018da013e3fa77a1a786c6d168
arturo <dace0015@algonquinlive.com>
2025-11-09 22:06:57 -0500

    a lot of mdn and looking through notes. settling on ideas on how to handle the promise

--- js/main.js
+++ js/main.js
@@ -14,5 +14,18 @@ function addListeners() {
 function yesNo(ev) {
 //   return Math.round(Math.random()) === 1 ? true : false;
 // log(Math.round(Math.random()) === 1 ? true : false)
-log(Math.round(Math.random()))
+const oneOrZero = Math.round(Math.random())
+// log(oneOrZero)
+// log(getRandomInt(1000, 2001))
+new Promise((leftPath, rightPath)=>{
+    setTimeout(()=> {
+        
+    })
+})
+}
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
