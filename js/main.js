0ef23e0c9f37717a3dace3bdf511b4940b015e1b
arturo <dace0015@algonquinlive.com>
2025-11-09 22:51:25 -0500

    Houston, we have background colors. Now I gotta maybe move things to a util and take functions out of yesNo and just call on them... also do the main part

--- js/main.js
+++ js/main.js
@@ -17,15 +17,29 @@ function yesNo(ev) {
 const oneOrZero = Math.round(Math.random())
 // log(oneOrZero)
 // log(getRandomInt(1000, 2001))
-new Promise((leftPath, rightPath)=>{
-    setTimeout(()=> {
-        
+if (oneOrZero === 1) {
+    log("color added to body")
+    new Promise((resolve)=>{
+        setTimeout(()=>{
+            const bodyHexColor = randomColour();
+            log(`${bodyHexColor}`)
+            log (`background-color: ${bodyHexColor};`)
+            const bodyBackgroundColor = `background-color: ${bodyHexColor};`
+            document.querySelector("body").style.backgroundColor = `${bodyHexColor}`
+        }, getRandomInt(1000, 2001))
     })
-})
+} else {
+    log("text added to main")
+}
 }
 
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+function randomColour () {
+    //return a random 6 digit hex colour
+    return `#` + Math.random().toString(16).substring(2, 8);
 }
